da4791fb803890d5b62fe9f6e5dcc4832d50571b
Luiz Fernando <luizfernandoargento@gmail.com>
2021-07-22 02:25:17 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,38 @@
+const { createCipher } = require('crypto')
+const os = require('os')
+// Importar o comando "os" do NODE.
+
+// console.log(os.platform())
+// Quando executado no console, é o mesmo que usar no terminal "node os.platform()"
+
+setInterval(() => {
+
+    const { freemem, totalmem } = os
+    // Extrai os comandos "freemem" e "totalmem" do "os", que são referentes a memória do sistema.
+    // Mais fácil que usar: "const freemem = os.freemem
+
+    //console.log( `${parseInt(freemem() / 1024 / 1024)} MB`, totalmem())
+    // Converteu para inteiro e dividiu bytes em megabytes.
+    // Usou template literals entre crase "`" (acento invertido) e $ para usar javascript em string.
+
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((mem / total) * 100)
+
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`,
+    }
+
+    console.clear()
+    // Limpa a tela
+    console.log("   === PC STATS ===");
+    console.table(stats);
+    // Mostra no console em forma de tabela.
+
+}, 1000)
+// Arrow Function resume a função anônima "function() {...}" para "() => {...}"
+// setInterval = função global que executa uma função a cada tempo, no caso 1 segundo (1000).
+
+
